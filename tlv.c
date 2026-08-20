mlichvar/linuxptp

commit a0154d343e7665c97652b9416e6b9576277c6231

Add management message for timestamp and delay corrections.
Add a management message to get and set the port egressLatency,
ingressLatency and delayAsymmetry variables. This enables an external
process to compensate for changes in the network and timestamping
asymmetries while ptp4l is running.

Exchange the values as fractional nanoseconds, but parse and print them
in pmc as whole nanoseconds.

Signed-off-by: Miroslav Lichvar <mlichvar@redhat.com>

tlv.c
@@ -174,6 +174,7 @@ static int mgt_post_recv(struct management_tlv *m, uint16_t data_len,
 	struct mgmt_clock_description *cd;
 	struct unicast_master_entry *ume;
 	struct subscribe_events_np *sen;
+	struct port_corrections_np *pcn;
 	struct port_properties_np *ppn;
 	struct port_hwclock_np *phn;
 	struct timePropertiesDS *tp;
@@ -497,6 +498,14 @@ static int mgt_post_recv(struct management_tlv *m, uint16_t data_len,
 		egpn = (struct external_grandmaster_properties_np *) m->data;
 		NTOHS(egpn->stepsRemoved);
 		break;
+	case MID_PORT_CORRECTIONS_NP:
+		if (data_len != sizeof(struct port_corrections_np))
+			goto bad_length;
+		pcn = (struct port_corrections_np *) m->data;
+		net2host64(pcn->egressLatency);
+		net2host64(pcn->ingressLatency);
+		net2host64(pcn->delayAsymmetry);
+		break;
 	case MID_SAVE_IN_NON_VOLATILE_STORAGE:
 	case MID_RESET_NON_VOLATILE_STORAGE:
 	case MID_INITIALIZE:
@@ -529,6 +538,7 @@ static void mgt_pre_send(struct management_tlv *m, struct tlv_extra *extra)
 	struct mgmt_clock_description *cd;
 	struct unicast_master_entry *ume;
 	struct subscribe_events_np *sen;
+	struct port_corrections_np *pcn;
 	struct port_properties_np *ppn;
 	struct port_hwclock_np *phn;
 	struct cmlds_info_np *cmlds;
@@ -699,6 +709,12 @@ static void mgt_pre_send(struct management_tlv *m, struct tlv_extra *extra)
 	case MID_EXTERNAL_GRANDMASTER_PROPERTIES_NP:
 		egpn = (struct external_grandmaster_properties_np *)m->data;
 		HTONS(egpn->stepsRemoved);
+		break;
+	case MID_PORT_CORRECTIONS_NP:
+		pcn = (struct port_corrections_np *)m->data;
+		host2net64(pcn->egressLatency);
+		host2net64(pcn->ingressLatency);
+		host2net64(pcn->delayAsymmetry);
 		break;
 	}
 }

pmc.c
@@ -169,6 +169,7 @@ static void pmc_show(struct ptp_message *msg, FILE *fp)
 	struct management_tlv_datum *mtd;
 	struct unicast_master_entry *ume;
 	struct subscribe_events_np *sen;
+	struct port_corrections_np *pcn;
 	struct port_properties_np *ppn;
 	struct port_hwclock_np *phn;
 	struct cmlds_info_np *cmlds;
@@ -653,6 +654,16 @@ static void pmc_show(struct ptp_message *msg, FILE *fp)
 			cmlds->meanLinkDelay >> 16,
 			cmlds->scaledNeighborRateRatio,
 			cmlds->as_capable);
+		break;
+	case MID_PORT_CORRECTIONS_NP:
+		pcn = (struct port_corrections_np *) mgt->data;
+		fprintf(fp, "PORT_CORRECTIONS_NP "
+			IFMT "egressLatency  %"PRId64" "
+			IFMT "ingressLatency %"PRId64" "
+			IFMT "delayAsymmetry %"PRId64" ",
+			pcn->egressLatency >> 16,
+			pcn->ingressLatency >> 16,
+			pcn->delayAsymmetry >> 16);
 		break;
 	case MID_LOG_ANNOUNCE_INTERVAL:
 		mtd = (struct management_tlv_datum *) mgt->data;

pmc_common.c
@@ -159,6 +159,7 @@ struct management_id idtab[] = {
 	{ "PORT_HWCLOCK_NP", MID_PORT_HWCLOCK_NP, do_get_action },
 	{ "POWER_PROFILE_SETTINGS_NP", MID_POWER_PROFILE_SETTINGS_NP, do_set_action },
 	{ "CMLDS_INFO_NP", MID_CMLDS_INFO_NP, do_get_action },
+	{ "PORT_CORRECTIONS_NP", MID_PORT_CORRECTIONS_NP, do_set_action },
 };
 
 static void do_get_action(struct pmc *pmc, int action, int index, char *str)
@@ -179,6 +180,7 @@ static void do_set_action(struct pmc *pmc, int action, int index, char *str)
 	struct grandmaster_settings_np gsn;
 	struct management_tlv_datum mtd;
 	struct subscribe_events_np sen;
+	struct port_corrections_np pcn;
 	struct port_ds_np pnp;
 	char onoff_port_state[4] = "off";
 	char onoff_time_status[4] = "off";
@@ -415,6 +417,24 @@ static void do_set_action(struct pmc *pmc, int action, int index, char *str)
 			break;
 		}
 		pmc_send_set_action(pmc, code, &egpn, sizeof(egpn));
+		break;
+	case MID_PORT_CORRECTIONS_NP:
+		cnt = sscanf(str, " %*s %*s "
+			     "egressLatency  %"SCNd64" "
+			     "ingressLatency %"SCNd64" "
+			     "delayAsymmetry %"SCNd64" ",
+			     &pcn.egressLatency,
+			     &pcn.ingressLatency,
+			     &pcn.delayAsymmetry);
+		if (cnt != 3) {
+			fprintf(stderr, "%s SET needs 3 values\n",
+				idtab[index].name);
+			break;
+		}
+		pcn.egressLatency <<= 16;
+		pcn.ingressLatency <<= 16;
+		pcn.delayAsymmetry <<= 16;
+		pmc_send_set_action(pmc, code, &pcn, sizeof(pcn));
 		break;
 	}
 }
@@ -722,6 +742,9 @@ static int pmc_tlv_datalen(struct pmc *pmc, int id)
 		break;
 	case MID_EXTERNAL_GRANDMASTER_PROPERTIES_NP:
 		len += sizeof(struct external_grandmaster_properties_np);
+		break;
+	case MID_PORT_CORRECTIONS_NP:
+		len += sizeof(struct port_corrections_np);
 		break;
 	case MID_LOG_ANNOUNCE_INTERVAL:
 	case MID_ANNOUNCE_RECEIPT_TIMEOUT:

port.c
@@ -913,6 +913,7 @@ static int port_management_fill_response(struct port *target,
 	struct management_tlv_datum *mtd;
 	struct unicast_master_entry *ume;
 	struct clock_description *desc;
+	struct port_corrections_np *pcn;
 	struct port_properties_np *ppn;
 	struct port_hwclock_np *phn;
 	struct cmlds_info_np *cmlds;
@@ -1170,6 +1171,13 @@ static int port_management_fill_response(struct port *target,
 		cmlds->as_capable = target->asCapable;
 		datalen = sizeof(*cmlds);
 		break;
+	case MID_PORT_CORRECTIONS_NP:
+		pcn = (struct port_corrections_np *)tlv->data;
+		pcn->egressLatency = target->tx_timestamp_offset;
+		pcn->ingressLatency = target->rx_timestamp_offset;
+		pcn->delayAsymmetry = target->asymmetry;
+		datalen = sizeof(*pcn);
+		break;
 	default:
 		/* The caller should *not* respond to this message. */
 		tlv_extra_recycle(extra);
@@ -1212,6 +1220,7 @@ static int port_management_set(struct port *target,
 			       struct ptp_message *req)
 {
 	struct ieee_c37_238_settings_np *pwr;
+	struct port_corrections_np *pcn;
 	struct management_tlv *tlv;
 	struct port_ds_np *pdsnp;
 	int respond = 0;
@@ -1234,6 +1243,13 @@ static int port_management_set(struct port *target,
 			respond = 1;
 			break;
 		}
+		break;
+	case MID_PORT_CORRECTIONS_NP:
+		pcn = (struct port_corrections_np *) tlv->data;
+		target->tx_timestamp_offset = pcn->egressLatency;
+		target->rx_timestamp_offset = pcn->ingressLatency;
+		target->asymmetry = pcn->delayAsymmetry;
+		respond = 1;
 		break;
 	}
 	if (respond && !port_management_get_response(target, ingress, id, req))

tlv.h
@@ -131,6 +131,7 @@ enum management_action {
 #define MID_PORT_HWCLOCK_NP				0xC009
 #define MID_POWER_PROFILE_SETTINGS_NP			0xC00A
 #define MID_CMLDS_INFO_NP				0xC00B
+#define MID_PORT_CORRECTIONS_NP				0xC00C
 
 /* Management error ID values */
 #define MID_RESPONSE_TOO_BIG				0x0001
@@ -493,6 +494,12 @@ struct msg_interface_rate_tlv {
 struct external_grandmaster_properties_np {
 	struct ClockIdentity gmIdentity;
 	UInteger16 stepsRemoved;
+} PACKED;
+
+struct port_corrections_np {
+	Integer64 egressLatency;
+	Integer64 ingressLatency;
+	Integer64 delayAsymmetry;
 } PACKED;
 
 /**

commit e8268b1b26a7e5647536bdf896bec2015cdc75b5

tlv: Fix network/host conversion of PORT_CORRECTIONS_NP.
The net2host64() and host2net64() functions return the converted value
without modifying the value in memory (unlike the _unaligned variants).
The PORT_CORRECTIONS_NP-specific code didn't store the returned values,
i.e. they were sent over network in the native host byte order.

Fix the code to actually use the converted values to always send and
receive them in network order.

Fixes: a0154d343e76 ("Add management message for timestamp and delay corrections.")
Reported-by: Richard Cochran <richardcochran@gmail.com>
Signed-off-by: Miroslav Lichvar <mlichvar@redhat.com>

tlv.c
@@ -504,9 +504,9 @@ static int mgt_post_recv(struct management_tlv *m, uint16_t data_len,
 		if (data_len != sizeof(struct port_corrections_np))
 			goto bad_length;
 		pcn = (struct port_corrections_np *) m->data;
-		net2host64(pcn->egressLatency);
-		net2host64(pcn->ingressLatency);
-		net2host64(pcn->delayAsymmetry);
+		pcn->egressLatency = net2host64(pcn->egressLatency);
+		pcn->ingressLatency = net2host64(pcn->ingressLatency);
+		pcn->delayAsymmetry = net2host64(pcn->delayAsymmetry);
 		break;
 	case MID_SERVO_STATUS_NP:
 		if (data_len != sizeof(struct servo_status_np))
@@ -734,9 +734,9 @@ static void mgt_pre_send(struct management_tlv *m, struct tlv_extra *extra)
 		break;
 	case MID_PORT_CORRECTIONS_NP:
 		pcn = (struct port_corrections_np *)m->data;
-		host2net64(pcn->egressLatency);
-		host2net64(pcn->ingressLatency);
-		host2net64(pcn->delayAsymmetry);
+		pcn->egressLatency = host2net64(pcn->egressLatency);
+		pcn->ingressLatency = host2net64(pcn->ingressLatency);
+		pcn->delayAsymmetry = host2net64(pcn->delayAsymmetry);
 		break;
 	case MID_SERVO_STATUS_NP:
 		ssn = (struct servo_status_np *)m->data;